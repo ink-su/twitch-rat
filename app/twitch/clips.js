d153752d5ede0db548250e55d5f0b1e280493c7b
ink-su <6701545+ink-su@users.noreply.github.com>
2018-07-03 02:42:34 -0400

    Fixed eslint, cleaned code

--- app/twitch/clips.js
+++ app/twitch/clips.js
@@ -1,6 +1,8 @@
 'use strict';
+
 const bluebird = require('bluebird');
 const nconf = require('nconf');
+const path = require('path');
 const fs = require('fs');
 const req = require('request');
 
@@ -8,22 +10,22 @@ const req = require('request');
  * Returns a list of top clips on twitch
  */
 function getClips() {
-  let game = 'League of Legends'
-  let limit = 10;
-  let period = 'day';
-  let language = "en";
-  let id = nconf.get("twitchclient");
-  let options = {
+  const game = 'League of Legends';
+  const limit = 10;
+  const period = 'day';
+  const language = 'en';
+  const id = nconf.get('twitchclient');
+  const options = {
     method: 'get',
     url: `https://api.twitch.tv/kraken/clips/top?game=${game}&limit=${limit}&period=${period}&language=${language}`,
     json: true,
     headers: {
-      "Client-ID": id,
-      "Accept": "application/vnd.twitchtv.v5+json"
-    }
+      'Client-ID': id,
+      Accept: 'application/vnd.twitchtv.v5+json',
+    },
   };
-  return new bluebird.Promise((resolve, reject) => {
-    req(options, function (error, response, body) {
+  return new bluebird.Promise((resolve) => {
+    req(options, (error, response, body) => {
       resolve(body);
     });
   });
@@ -31,13 +33,13 @@ function getClips() {
 
 /**
  * Returns the filename of a clip
- * @param {clip} clip 
+ * @param {clip} clip
  */
 function getFileName(clip) {
-  let img = clip.thumbnails.medium;
-  let start = img.lastIndexOf('\/') + 1;
-  let end = img.lastIndexOf('-preview-');
-  return img.substring(start, end) + ".mp4";
+  const img = clip.thumbnails.medium;
+  const start = img.lastIndexOf('/') + 1;
+  const end = img.lastIndexOf('-preview-');
+  return `${img.substring(start, end)}.mp4`;
 }
 
 /**
@@ -47,25 +49,25 @@ function getClipFiles() {
   return getClips()
     .then(body => body.clips)
     .map(clip => ({
-        id: clip.tracking_id,
-        title: clip.title,
-        poster: clip.broadcaster.display_name,
-        duration: clip.duration,
-        views: clip.views,
-        filename: getFileName(clip),
-      }))
+      id: clip.tracking_id,
+      title: clip.title,
+      poster: clip.broadcaster.display_name,
+      duration: clip.duration,
+      views: clip.views,
+      filename: getFileName(clip),
+    }));
 }
 /**
  * Download clip to data directory
- * @param {string} name  
+ * @param {string} name
  */
 function saveClip(name) {
-  let url = `https://clips-media-assets2.twitch.tv/${name}`;
-  return req(url).pipe(fs.createWriteStream(__dirname + `/../../data/${name}.mp4`));
+  const url = `https://clips-media-assets2.twitch.tv/${name}`;
+  return req(url).pipe(fs.createWriteStream(path.join(__dirname, `../../data/${name}.mp4`)));
 }
 
 module.exports = {
   getClips,
   getClipFiles,
   saveClip,
-}
+};
